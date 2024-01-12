73d1eed74abd2d038b5d0bf77e0f05b270b2cecc
rgeorge96 <156020911+rgeorge96@users.noreply.github.com>
2024-01-12 14:06:15 -0800

    added index.html

---
+++ script.js
@@ -0,0 +1,8 @@
+// Select the scroll-top button
+const scrollTopButton = document.getElementById("scroll-top");
+
+// Add an event listener to the button
+scrollTopButton.addEventListener("click", () => {
+    // Scroll to the top of the page
+    window.scrollTo(0, 0);
+});
